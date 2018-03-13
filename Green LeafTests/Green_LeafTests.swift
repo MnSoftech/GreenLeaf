854d89ea90fdfb6f0aee0a11c3f0b89ed84db3fb
MnSoftech <mnsoftech11@gmail.com>
2018-03-13 11:59:09 +0500

    Initial Commit

---
+++ Green LeafTests/Green_LeafTests.swift
@@ -0,0 +1,36 @@
+//
+//  Green_LeafTests.swift
+//  Green LeafTests
+//
+//  Created by Muhammad Noaman on 13/03/2018.
+//  Copyright © 2018 MnSoftech. All rights reserved.
+//
+
+import XCTest
+@testable import Green_Leaf
+
+class Green_LeafTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
